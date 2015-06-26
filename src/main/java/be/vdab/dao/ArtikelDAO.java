5555dba95a3156f2027f257536d35e694e7a840a
TimothyPecceu <timothy.pecceu@gmail.com>
2015-06-26 11:54:34 +0200

    Implemented ThreadLocal

--- src/main/java/be/vdab/dao/ArtikelDAO.java
+++ src/main/java/be/vdab/dao/ArtikelDAO.java
@@ -5,13 +5,13 @@ import javax.persistence.EntityManager;
 import be.vdab.entities.Artikel;
 import be.vdab.filters.JPAFilter;
 
-public class ArtikelDAO {
+public class ArtikelDAO extends AbstractDAO{
 
-	public Artikel read(long id, EntityManager entityManager) {
-		return entityManager.find(Artikel.class, id);
+	public Artikel read(long id) {
+		return getEntityManager().find(Artikel.class, id);
 	}
 	
-	public void create(Artikel artikel, EntityManager entityManager){
-		entityManager.persist(artikel);
+	public void create(Artikel artikel){
+		getEntityManager().persist(artikel);
 	}
 }
--- src/main/java/be/vdab/services/ArtikelService.java
+++ src/main/java/be/vdab/services/ArtikelService.java
@@ -1,36 +1,19 @@
 package be.vdab.services;
 
-import javax.persistence.EntityManager;
-
 import be.vdab.dao.ArtikelDAO;
 import be.vdab.entities.Artikel;
-import be.vdab.filters.JPAFilter;
 
 public class ArtikelService {
 
 	private final ArtikelDAO artikelDAO = new ArtikelDAO();
 	
 	public Artikel read(long id){
-		EntityManager entityManager = JPAFilter.getEntityManager();
-		try {
-			return artikelDAO.read(id,entityManager);
-		} finally {
-			entityManager.close();
-		}
+		return artikelDAO.read(id);
 	}
 	
 	public void create(Artikel artikel){
-		EntityManager entityManager = JPAFilter.getEntityManager();
-		
-		try{
-			entityManager.getTransaction().begin();
-			artikelDAO.create(artikel, entityManager);
-			entityManager.getTransaction().commit();
-		}catch (RuntimeException ex){
-			entityManager.getTransaction().rollback();
-			throw ex;
-		}finally{
-			entityManager.close();
-		}
+		artikelDAO.beginTransaction();
+		artikelDAO.create(artikel);
+		artikelDAO.commit();
 	}
 }
